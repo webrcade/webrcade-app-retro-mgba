3e4b1c8cb0f417c6602b54f00ba86ae5c67e3330
raz0red <raz0red@gmail.com>
2025-12-14 11:48:45 -0800

    rotation

--- src/emulator/index.js
+++ src/emulator/index.js
@@ -14,6 +14,9 @@ export class Emulator extends RetroAppWrapper {
     super(app, debug);
     window.emulator = this;
 
+    this.rotValue = 0;
+    this.rotSideways = false;
+
     this.audioStarted = 0;
     this.audioCallback = (offset, length) => {
       length = length << 1;
@@ -52,52 +55,57 @@ export class Emulator extends RetroAppWrapper {
     return this.prefs;
   }
 
-  // sendInput(controller, input, analog0x, analog0y, analog1x, analog1y) {
-  //   const { controllers } = this;
+  sendInput(controller, input, analog0x, analog0y, analog1x, analog1y) {
+    const rotValue = this.rotValue;
+    if (this.rotValue !== 0) {
+      const isUp    = input & this.INP_UP;
+      const isRight = input & this.INP_RIGHT;
+      const isDown  = input & this.INP_DOWN;
+      const isLeft  = input & this.INP_LEFT;
 
-  //   if (controllers.isControlDown(0, CIDS.SELECT)) {
-  //     this.selectDown = true;
-  //   } else {
-  //     if (this.selectDown) {
-  //       this.rotated = !this.rotated;
-  //       this.resizeScreen(this.canvas);
-  //       this.selectDown = false;
-  //     }
-  //   }
+      input = input & ~(this.INP_LEFT | this.INP_RIGHT | this.INP_UP | this.INP_DOWN); // clear D-pad bits
 
-  //   let isLeft = false;
-  //   let isRight = false;
-  //   let isUp = false;
-  //   let isDown = false;
+      switch (rotValue) {
+        case 1: // 90° CW
+          if (isUp)    input |= this.INP_LEFT;
+          if (isRight) input |= this.INP_UP;
+          if (isDown)  input |= this.INP_RIGHT;
+          if (isLeft)  input |= this.INP_DOWN;
+          break;
 
-  //   if (controllers.isAxisLeft(0, 1)) {
-  //     isLeft = true;
-  //   }
-  //   if (controllers.isAxisRight(0, 1)) {
-  //     isRight = true;
-  //   }
-  //   if (controllers.isAxisUp(0, 1)) {
-  //     isUp = true;
-  //   }
-  //   if (controllers.isAxisDown(0, 1)) {
-  //     isDown = true;
-  //   }
+        case 2: // 180°
+          if (isUp)    input |= this.INP_DOWN;
+          if (isRight) input |= this.INP_LEFT;
+          if (isDown)  input |= this.INP_UP;
+          if (isLeft)  input |= this.INP_RIGHT;
+          break;
 
-  //   if (!this.getDisableInput()) {
-  //     window.Module._wrc_set_input(
-  //       controller,
-  //       input,
-  //       analog0x,
-  //       analog0y,
-  //       isLeft ? .25 : isRight ? .5 : 0,
-  //       isUp ? .25 : isDown ? .5 : 0,
-  //     );
-  //   } else {
-  //     window.Module._wrc_set_input(
-  //       controller, 0, 0, 0, 0, 0
-  //     );
-  //   }
-  // }
+        case 3: // 270° CW (or 90° CCW)
+          if (isUp)    input |= this.INP_RIGHT;
+          if (isRight) input |= this.INP_DOWN;
+          if (isDown)  input |= this.INP_LEFT;
+          if (isLeft)  input |= this.INP_UP;
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (!this.getDisableInput()) {
+      window.Module._wrc_set_input(
+        controller,
+        input,
+        analog0x,
+        analog0y,
+        analog1x,
+        analog1y,
+      );
+    } else {
+      window.Module._wrc_set_input(
+        controller, 0, 0, 0, 0, 0
+      );
+    }
+  }
 
   async saveState() {
     const { saveStatePath, started } = this;
@@ -201,8 +209,26 @@ export class Emulator extends RetroAppWrapper {
   }
 
   applyGameSettings() {
-    // const { Module } = window;
-    // const props = this.getProps();
+    const props = this.getProps();
+
+    // Get the ROM rotation (if applicable)
+    const rot = props.rotation;
+    if (rot) {
+      const rotInt = parseInt(rot);
+      if (!isNaN(rotInt)) {
+        if (rotInt % 90 === 0) {
+          this.rotValue = (rotInt / 90) % 4;
+          if (this.rotValue % 2 !== 0) {
+            this.rotSideways = true;
+          }
+        } else {
+          LOG.error('rotation value is not a 90 degree value: ' + rot);
+        }
+      } else {
+        LOG.error('rotation value is not a number: ' + rot);
+      }
+    }
+
     // let options = 0;
 
     // // b buttons
@@ -224,24 +250,64 @@ export class Emulator extends RetroAppWrapper {
     // Module._wrc_set_options(options);
   }
 
+  updateScreenSize() {
+    super.updateScreenSize()
+
+    const canvas = this.canvas;
+    if (canvas) {
+      if (this.rotValue === 1 || this.rotValue === 3) {
+        const w = canvas.width;
+        const h = canvas.height;
+        const vw = window.innerWidth;
+        const vh = window.innerHeight;
+
+        // Calculate scaling factor depending on rotation
+        const rotated = true;
+        const cw = rotated ? h : w;
+        const ch = rotated ? w : h;
+
+        // Scale to fit inside viewport
+        const scale = Math.min(vw / cw, vh / ch);
+
+        canvas.style.setProperty(
+          'transform',
+          `rotate(${this.rotValue * 90}deg) scale(${scale})`,
+          'important'
+        );
+      }
+    }
+  }
+
+  isDisableLookup() {
+    const props = this.getProps();
+    return props.disableLookup ? 1 : 0;
+  }
+
+  enableRTC() {
+    const props = this.getProps();
+    return props.rtc ? 1 : 0;
+  }
+
+  saveType() {
+    const props = this.getProps();
+    return props.saveType ? props.saveType : 0;
+  }
+
+  flashSize() {
+    const props = this.getProps();
+    return props.flashSize ? props.flashSize : (64 * 1024);
+  }
+
   isForceAspectRatio() {
     return false;
   }
 
-  // isRotated() {
-  //   return this.rotated;
-  // }
-
-  // isJapanese() {
-  //   return this.japanese;
-  // }
-
   getDefaultAspectRatio() {
-    let ar = 1.5;
+    return 1.5;
     // if (!this.isGba) {
     //   ar = this.gbBorder ? 1.15 : 1.11;
     // }
-    return ar;
+    // return ar;
   }
 
   resizeScreen(canvas) {
